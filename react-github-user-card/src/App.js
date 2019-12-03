be5ce38f63113ac25279ce49093500a37c6fdeb3
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2019-12-03 15:11:13 -0700

    Struggling

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import User from "./components/User";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>GitHub User Card</h1>
+			<User />
+		</div>
+	);
+}
+
+export default App;
---
+++ react-github-user-card/src/components/User.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Axios from "axios";
+import UserCard from "./UserCard";
+
+class User extends React.Component {
+	state = {
+		name: []
+	};
+
+	componentDidMount() {
+		Axios.get("https://api.github.com/users/redfordch1").then((response) => {
+			console.log(response);
+			this.setState({
+				name: response.data.name,
+				img: response.data.avatar_url,
+				bio: response.data.bio
+			});
+		});
+	}
+}
+
+export default User;
